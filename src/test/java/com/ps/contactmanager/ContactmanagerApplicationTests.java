be3c01c5d4cf223fd1df31ebf79d9d17f98d30cc
Abdelwakil RABIDI <abdelwakil.rabidi@beyn.io>
2023-05-04 00:04:52 +0100

    Initial commit

---
+++ src/test/java/com/ps/contactmanager/ContactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
